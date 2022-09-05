7ca445a4cad439ec0fbafda8ff556527ebd62789
Pol Henarejos <pol.henarejos@cttc.es>
2022-09-05 17:05:33 +0200

    Adding version file.
    Signed-off-by: Pol Henarejos <pol.henarejos@cttc.es>

---
+++ src/fido/version.h
@@ -0,0 +1,27 @@
+/*
+ * This file is part of the Pico FIDO distribution (https://github.com/polhenarejos/pico-fido).
+ * Copyright (c) 2022 Pol Henarejos.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __VERSION_H_
+#define __VERSION_H_
+
+#define PICO_FIDO_VERSION 0x0100
+
+#define PICO_FIDO_VERSION_MAJOR ((PICO_FIDO_VERSION >> 8) & 0xff)
+#define PICO_FIDO_VERSION_MINOR (PICO_FIDO_VERSION & 0xff)
+
+#endif
+
