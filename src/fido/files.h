004073c3dd1a1c0c0b92f2c2274843050eb92288
Pol Henarejos <pol.henarejos@cttc.es>
2022-12-01 19:20:19 +0100

    Adding FID for Enterprise certificate.
    Signed-off-by: Pol Henarejos <pol.henarejos@cttc.es>

--- src/fido/files.h
+++ src/fido/files.h
@@ -23,6 +23,7 @@
 #define EF_KEY_DEV      0xCC00
 #define EF_KEY_DEV_ENC  0xCC01
 #define EF_EE_DEV       0xCE00
+#define EF_EE_DEV_EA    0xCE01
 #define EF_COUNTER      0xC000
 #define EF_OPTS         0xC001
 #define EF_PIN          0x1080
