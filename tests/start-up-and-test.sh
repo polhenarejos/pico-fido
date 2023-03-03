12250a1c3170d5d89763b882d85e0a179e178317
Pol Henarejos <pol.henarejos@cttc.es>
2023-03-03 17:54:00 +0100

    Small fixes.
    Signed-off-by: Pol Henarejos <pol.henarejos@cttc.es>

--- tests/start-up-and-test.sh
+++ tests/start-up-and-test.sh
@@ -3,6 +3,6 @@
 /usr/sbin/pcscd &
 sleep 2
 rm -f memory.flash
-cp -R tests/Docker/fido2/* /usr/local/lib/python3.9/dist-packages/fido2/hid
+cp -R tests/docker/fido2/* /usr/local/lib/python3.9/dist-packages/fido2/hid
 ./build_in_docker/pico_fido > /dev/null &
 pytest tests
