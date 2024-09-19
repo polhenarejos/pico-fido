e05115ffaca4fbbf3b3414b697be5c4a2ce3075b
Pol Henarejos <pol.henarejos@cttc.es>
2024-09-19 19:37:01 +0200

    Fix autobuild for ESP32.
    Signed-off-by: Pol Henarejos <pol.henarejos@cttc.es>

--- workflows/autobuild_esp32.sh
+++ workflows/autobuild_esp32.sh
@@ -2,16 +2,6 @@
 
 git submodule update --init --recursive
 sudo apt update
-sudo apt install -y cmake gcc-arm-none-eabi libnewlib-arm-none-eabi libstdc++-arm-none-eabi-newlib
-git clone https://github.com/raspberrypi/pico-sdk
-cd pico-sdk
-git submodule update --init
-cd ..
-mkdir build
-cd build
-cmake -DPICO_SDK_PATH=../pico-sdk ..
-make
-
 sudo apt install -y git wget flex bison gperf python3 python3-pip python3-venv cmake ninja-build ccache libffi-dev libssl-dev dfu-util libusb-1.0-0
 git clone --recursive https://github.com/espressif/esp-idf.git
 cd esp-idf
