53ed3a46c41912a302e238dcef680d3fad9c7f65
Pol Henarejos <pol.henarejos@cttc.es>
2024-10-01 09:34:22 +0200

    Add autobuild for local.
    Harmonize with other repos.
    
    Signed-off-by: Pol Henarejos <pol.henarejos@cttc.es>

--- workflows/autobuild.sh
+++ workflows/autobuild.sh
@@ -2,7 +2,9 @@
 
 git submodule update --init --recursive
 sudo apt update
-sudo apt install -y cmake gcc-arm-none-eabi libnewlib-arm-none-eabi libstdc++-arm-none-eabi-newlib build-essential pkg-config libusb-1.0-0-dev
+
+if [[ $1 == "pico" ]]; then
+sudo apt install -y cmake gcc-arm-none-eabi libnewlib-arm-none-eabi libstdc++-arm-none-eabi-newlib
 git clone https://github.com/raspberrypi/pico-sdk
 cd pico-sdk
 git submodule update --init
@@ -19,4 +21,19 @@ cd ../..
 mkdir build_pico
 cd build_pico
 cmake -DPICO_SDK_PATH=../pico-sdk ..
-make -j`nproc`
+make
+elif [[ $1 == "esp32" ]]; then
+sudo apt install -y git wget flex bison gperf python3 python3-pip python3-venv cmake ninja-build ccache libffi-dev libssl-dev dfu-util libusb-1.0-0
+git clone --recursive https://github.com/espressif/esp-idf.git
+cd esp-idf
+./install.sh esp32s3
+. ./export.sh
+cd ..
+idf.py set-target esp32s3
+idf.py all
+else
+mkdir build
+cd build
+cmake -DENABLE_EMULATION=1 ..
+make
+fi
