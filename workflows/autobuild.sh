7800056597c910b549e4867cf3e358313e6a5f93
Pol Henarejos <pol.henarejos@cttc.es>
2024-12-27 02:23:11 +0100

    Fix bin name.
    Signed-off-by: Pol Henarejos <pol.henarejos@cttc.es>

--- workflows/autobuild.sh
+++ workflows/autobuild.sh
@@ -35,7 +35,7 @@ idf.py set-target esp32s3
 idf.py all
 mkdir -p release
 cd build
-esptool.py --chip ESP32-S3 merge_bin -o ../release/esp32-s3.bin @flash_args
+esptool.py --chip ESP32-S3 merge_bin -o ../release/pico_fido_esp32-s3.bin @flash_args
 cd ..
 else
 mkdir build
