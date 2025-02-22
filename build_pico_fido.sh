eb857df3e14732976b1b0607c52d107491d7ae7a
Pol Henarejos <pol.henarejos@cttc.es>
2025-02-23 00:56:27 +0100

    Fix build name.
    Signed-off-by: Pol Henarejos <pol.henarejos@cttc.es>

--- build_pico_fido.sh
+++ build_pico_fido.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 VERSION_MAJOR="6"
-VERSION_MINOR="4-eddsa1"
+VERSION_MINOR="4"
 SUFFIX="${VERSION_MAJOR}.${VERSION_MINOR}"
 #if ! [[ -z "${GITHUB_SHA}" ]]; then
 #    SUFFIX="${SUFFIX}.${GITHUB_SHA}"
