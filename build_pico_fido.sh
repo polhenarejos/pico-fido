d2f69932759642201e9b62a08b7c10b2dd2754c9
Oakwen <dai.tianwen@gmail.com>
2025-03-01 15:38:24 +0800

    Update build_pico_fido.sh

--- build_pico_fido.sh
+++ build_pico_fido.sh
@@ -17,7 +17,9 @@ for board in "$board_dir"/*
 do
     board_name="$(basename -- $board .h)"
     rm -rf *
-    PICO_SDK_PATH="${PICO_SDK_PATH}" cmake .. -DPICO_BOARD=$board_name
-    make -j`nproc`
-    mv pico_fido.uf2 ../release/pico_fido_$board_name-$SUFFIX.uf2
+    if [ "$board_name" == "waveshare_rp2350_zero" ]; then
+        PICO_SDK_PATH="${PICO_SDK_PATH}" cmake .. -DPICO_BOARD=$board_name
+        make -j`nproc`
+        mv pico_fido.uf2 ../release/pico_fido_$board_name-$SUFFIX.uf2
+    fi
 done
