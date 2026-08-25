969a5227eb1e24b9a71af25b69da5edb14542a76
Pol Henarejos <pol.henarejos@cttc.es>
2026-08-25 16:32:35 +0200

    Reset only resets FIDO
    Signed-off-by: Pol Henarejos <pol.henarejos@cttc.es>

--- src/fido/cbor_reset.c
+++ src/fido/cbor_reset.c
@@ -29,6 +29,66 @@
 #include "fs/phy.h"
 #include "files.h"
 
+static bool fido_reset_should_clear(uint16_t fid) {
+    uint8_t prefix = (uint8_t)(fid >> 8);
+
+    switch (fid) {
+        case EF_KEY_DEV:
+        case EF_KEY_DEV_ENC:
+        case EF_EE_DEV:
+        case EF_EE_DEV_EA:
+        case EF_VAULT_KEY:
+        case EF_VAULT_LABEL:
+        case EF_COUNTER:
+        case EF_PIN:
+        case EF_AUTHTOKEN:
+        case EF_PAUTHTOKEN:
+        case EF_MINPINLEN:
+        case EF_PIN_COMPLEXITY_POLICY:
+        case EF_DEV_STATE:
+        case EF_OPTS:
+        case EF_LARGEBLOB:
+        case EF_PIN_ADMIN:
+            return true;
+        default:
+            break;
+    }
+
+    // FIDO vault, credential, and resident-object records use these dynamic FID prefixes.
+    return (prefix >= 0xc4 && prefix <= 0xc9) || prefix == 0xcf || (prefix >= 0xd0 && prefix <= 0xdc) || (prefix >= 0xe0 && prefix <= 0xe3);
+}
+
+typedef struct fido_reset_context {
+    int ret;
+} fido_reset_context_t;
+
+static bool fido_reset_dynamic_file(file_t *file, void *ctx) {
+    fido_reset_context_t *context = (fido_reset_context_t *)ctx;
+
+    if (!fido_reset_should_clear(file->fid)) {
+        return true;
+    }
+    context->ret = file_delete_no_commit(file);
+    return context->ret == PICOKEYS_OK;
+}
+
+static int fido_reset_storage(void) {
+    for (file_entry_t *entry = file_entries; entry != file_last; entry++) {
+        if (fido_reset_should_clear(entry->file.fid) && flash_clear_file(&entry->file) != PICOKEYS_OK) {
+            return PICOKEYS_EXEC_ERROR;
+        }
+    }
+
+    fido_reset_context_t context = { .ret = PICOKEYS_OK };
+    file_for_each_dynamic(fido_reset_dynamic_file, &context);
+    if (context.ret != PICOKEYS_OK) {
+        return context.ret;
+    }
+
+    flash_commit();
+    return PICOKEYS_OK;
+}
+
 int cbor_reset(void) {
 #ifndef ENABLE_EMULATION
 #if defined(ENABLE_POWER_ON_RESET) && ENABLE_POWER_ON_RESET == 1
@@ -44,7 +104,9 @@ int cbor_reset(void) {
         return CTAP2_ERR_OPERATION_DENIED;
     }
 #endif
-    file_initialize_flash(true);
+    if (fido_reset_storage() != PICOKEYS_OK) {
+        return CTAP2_ERR_PROCESSING;
+    }
     init_fido();
 #ifdef DEFAULT_MCUV_NOT_REQUIRED
     set_opts(get_opts() | FIDO2_OPT_MCUV_NOTRQD);
